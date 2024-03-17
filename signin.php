cc05a803ee1f02c42ace02eb9cd645044ee8d65f
Alexis <ajcodalify@gmail.com>
2024-03-17 15:23:26 +0800

    alexis modification

--- signin.php
+++ signin.php
@@ -4,12 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In Page</title>
+    <link rel="stylesheet" href="libs/css/custom.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 
-<!-- dito ka gagawa ng code mo para sa sign in page -->
 
+<div class="container">
+    <div class="header">
+        <h1>
+            <img src="images/logo.png" alt="">Garments Production Management
+        </h1>
+    </div>
+    <form action="">
+        <div class="form-container">
+            <div class="form">
 
+                <div class="input-field">
+                    <input type="text" name="email_address" placeholder="Username or Email"><br>
+                    <input type="password" name="password" placeholder="Password"><br>
+                </div>
+                
+                <div class="actions">
+                    <button>Login</button><br>
+                    <span>Forgot Password? <a href="">HERE!</a></span><br>
+                </div>
+                <hr>
+                <span>Not yet Registered <a href="#">HERE!</a></span>
+            </div>
+        </div>
+    </form>
+
+</div>
     
 </body>
 </html>
